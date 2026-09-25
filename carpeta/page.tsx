c8efef6f46d4f69ffc55c605571ce5d55f43da38
jcmacosta <jcmacosta@gmail.com>
2026-09-25 12:45:57 -0300

    Delete carpeta directory

--- carpeta/page.tsx
+++
@@ -1,9 +0,0 @@
-import MugDesigner from "@/components/mug-designer"
-
-export default function Page() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-muted/30 p-5">
-      <MugDesigner />
-    </main>
-  )
-}
